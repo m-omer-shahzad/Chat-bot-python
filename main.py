9be124722ea5f3f7f6e4512509de873241cfbd33
omer shahzad <omer.shahzad@codedistrict.com>
2023-08-16 10:54:57 +0500

    enhance the code for bestway

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
-import sys
 import os
-from langchain.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader, CSVLoader
-
-from langchain.vectorstores import Chroma
-from langchain.chat_models import ChatOpenAI
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-
-from langchain.chains import RetrievalQA
-from langchain.llms import OpenAI
-
-
+import sys
 from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
+from langchain.document_loaders import (CSVLoader, Docx2txtLoader, PyPDFLoader, TextLoader)
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.llms import OpenAI
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from colors import green, white, yellow
+
 load_dotenv()
 
 os.environ["OPENAI_API_KEY"]
 
 documents = []
 
+# Function to load a document using a specific loader
 def generateDoc(file, loaderType):
     pdf_path = "./docs/" + file
     loader = loaderType(pdf_path)
     documents.extend(loader.load())
-    
-for file in os.listdir("docs"):
-    if file.endswith(".pdf"):
-        generateDoc(file, PyPDFLoader)
-    elif file.endswith('.docx') or file.endswith('.doc'):
-        generateDoc(file, Docx2txtLoader)
-    elif file.endswith('.txt'):
-        generateDoc(file, TextLoader)
-    elif file.endswith('.csv'):
-        generateDoc(file, CSVLoader)
 
+# Dictionary mapping file extensions to corresponding loader classes
+loaders = {
+    '.pdf': PyPDFLoader,
+    '.docx': Docx2txtLoader,
+    '.doc': Docx2txtLoader,
+    '.txt': TextLoader,
+    '.csv': CSVLoader
+}
+
+# Loop through the files in the directory
+for file in os.listdir("docs"):
+    filename, extension = os.path.splitext(file)
+    if extension in loaders:
+        loader = loaders[extension]
+        generateDoc(file, loader)
+
+# Split documents into smaller chunks for processing
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 documents = text_splitter.split_documents(documents)
 
+# Create a Chroma vector database from the documents using OpenAI embeddings
 vectordb = Chroma.from_documents(documents, embedding=OpenAIEmbeddings(), persist_directory="./data"  )
 vectordb.persist()
 
@@ -45,11 +51,6 @@ qa_bot = RetrievalQA.from_chain_type(
     return_source_documents=True
 )
 
-yellow = "\033[0;33m"
-green = "\033[0;32m"
-white = "\033[0;39m"
-
-chat_history = []
 print(f"{yellow}---------------------------------------------------------------------------------")
 print('Welcome to the DocBot. You are now ready to start interacting with your documents')
 print('---------------------------------------------------------------------------------')
@@ -62,4 +63,3 @@ while True:
         continue
     result = qa_bot({'query': query})
     print(f"{white}Answer: " + result["result"])
-    chat_history.append((query, result["result"]))
